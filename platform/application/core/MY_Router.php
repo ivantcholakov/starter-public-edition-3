96277861b359210128d2ccd6296181c6b3b7f168
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-01-18 15:05:13 +0200

    Code refactoring.

--- platform/application/core/MY_Router.php
+++ platform/application/core/MY_Router.php
@@ -1,10 +1,5 @@
 <?php (defined('BASEPATH')) OR exit('No direct script access allowed');
 
-/* load the MX_Router class */
-if (!class_exists('MX_Router', false)) {
-    require APPPATH.'third_party/MX/Router.php';
-}
-
 class MY_Router extends MX_Router {
 
     public function __construct() {
