d2699642913693a8ccd6df0722f9091e19dd8040
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-07-19 05:37:51 +0300

    MY_Model: More built-in observers have been added: 'created_by', 'updated_by', 'deleted_at', and 'deleted_by'.

--- platform/application/core/MY_Model.php
+++ platform/application/core/MY_Model.php
@@ -179,6 +179,13 @@ class MY_Model extends CI_Model
      */
     protected $_count_string = 'SELECT COUNT(*) AS ';
 
+    /**
+     * User ID getter for the observers 'created_by', 'updated_by' and 'deleted_by'.
+     * It should be a callable type (function() or array($object, 'method))
+     * without parameters. If it is not set, User ID is assumed to be null value.
+     */
+    protected $user_id_getter = NULL;
+
     /* --------------------------------------------------------------
      * GENERIC METHODS
      * ------------------------------------------------------------ */
@@ -1316,7 +1323,20 @@ class MY_Model extends CI_Model
      * ------------------------------------------------------------ */
 
     /**
-     * MySQL DATETIME created_at and updated_at
+     * For supporting the observers below, the table definition should
+     * contatin a part of or all the following definitions (MySQL sysntax),
+     * depending on which observers you choose to use:
+     *  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+     *  `created_by` int(11) unsigned NOT NULL DEFAULT '0',
+     *  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+     *  `updated_by` int(11) unsigned NOT NULL DEFAULT '0',
+     *  `deleted` tinyint(1) NOT NULL DEFAULT '0',
+     *  `deleted_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+     *  `deleted_by` int(11) unsigned NOT NULL DEFAULT '0',
+     */
+
+    /**
+     * A timestamp observer, 'before_create' only.
      */
     public function created_at($row)
     {
@@ -1332,6 +1352,9 @@ class MY_Model extends CI_Model
         return $row;
     }
 
+    /**
+     * A timestamp observer, 'before_create' and 'before_update' only.
+     */
     public function updated_at($row)
     {
         if (is_object($row))
@@ -1344,6 +1367,62 @@ class MY_Model extends CI_Model
         }
 
         return $row;
+    }
+
+    /**
+     * A timestamp observer, 'before_delete' only.
+     */
+    public function deleted_at($parameter)
+    {
+        if ($this->soft_delete)
+        {
+            $this->_database->set($this->_table.'.'.'deleted_at', date('Y-m-d H:i:s'));
+        }
+    }
+
+    /**
+     * A user identification observer, 'before_create' only.
+     */
+    public function created_by($row)
+    {
+        if (is_object($row))
+        {
+            $row->created_by = $this->_get_user_id();
+        }
+        else
+        {
+            $row['created_by'] = $this->_get_user_id();
+        }
+
+        return $row;
+    }
+
+    /**
+     * A user identification observer, 'before_create' and 'before_update' only.
+     */
+    public function updated_by($row)
+    {
+        if (is_object($row))
+        {
+            $row->updated_by = $this->_get_user_id();
+        }
+        else
+        {
+            $row['updated_by'] = $this->_get_user_id();
+        }
+
+        return $row;
+    }
+
+    /**
+     * A user identification observer, 'before_delete' only.
+     */
+    public function deleted_by($parameter)
+    {
+        if ($this->soft_delete)
+        {
+            $this->_database->set($this->_table.'.'.'deleted_by', $this->_get_user_id());
+        }
     }
 
     /**
@@ -2085,7 +2164,6 @@ class MY_Model extends CI_Model
     /**
      * Resets all internal state flags and temporary scope data>
      */
-
     protected function _reset_state()
     {
         $this->_with = array();
@@ -2096,6 +2174,21 @@ class MY_Model extends CI_Model
         $this->qb_as_sql = FALSE;
         $this->qb_distinct = FALSE;
         $this->_as_json = FALSE;
+    }
+
+    /**
+     * Returns the current user ID.
+     */
+    protected function _get_user_id()
+    {
+        if (is_callable($this->user_id_getter))
+        {
+            return is_array($this->user_id_getter)
+                    ? $this->user_id_getter[0]->{$this->user_id_getter[1]}()
+                    : $this->user_id_getter();
+        }
+
+        return NULL;
     }
 
 }
