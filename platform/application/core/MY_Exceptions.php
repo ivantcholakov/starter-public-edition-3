336e13ad4f760f6645ea32fc57cb0e6a0dd72367
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-10-30 03:34:02 +0200

    Dirty changes about showing/logging exceptions.

--- platform/application/core/MY_Exceptions.php
+++ platform/application/core/MY_Exceptions.php
@@ -55,4 +55,45 @@ class MY_Exceptions extends CI_Exceptions {
         exit(4); // EXIT_UNKNOWN_FILE
     }
 
+    // --------------------------------------------------------------------
+
+    public function show_exception(Exception $exception)
+    {
+        $templates_path = config_item('error_views_path');
+        if (empty($templates_path))
+        {
+            $templates_path = VIEWPATH.'errors'.DIRECTORY_SEPARATOR;
+        }
+
+        // Modified by Ivan Tcholakov, 30-OCT-2014.
+        //$message = $exception->getMessage();
+        $message = is_object($exception) ? $exception->getMessage() : null;
+        //
+        if (empty($message))
+        {
+            $message = '(null)';
+        }
+
+        if (is_cli())
+        {
+            $templates_path .= 'cli'.DIRECTORY_SEPARATOR;
+        }
+        else
+        {
+            set_status_header(500);
+            $templates_path .= 'html'.DIRECTORY_SEPARATOR;
+        }
+
+        if (ob_get_level() > $this->ob_level + 1)
+        {
+            ob_end_flush();
+        }
+
+        ob_start();
+        include($templates_path.'error_exception.php');
+        $buffer = ob_get_contents();
+        ob_end_clean();
+        echo $buffer;
+    }
+
 }
