92ab028c9fd66b408ad1c86831f0e5c183b87bc1
Ivan Tcholakov <ivantcholakov@gmail.com>
2014-11-03 23:50:48 +0200

    Adding "Google Maps JavaScript API v3 Demo".

---
+++ platform/application/modules/playground/controllers/Google_maps_v3_ajax_controller.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed.');
+
+class Google_maps_v3_ajax_controller extends Base_Ajax_Controller {
+
+    public function __construct() {
+
+        parent::__construct();
+
+        $this->load->config('db');
+        $this->load->database($this->config->item('db'), false, true);
+        $this->load->model('map');
+    }
+
+    public function index() {
+
+        $this->output->set_header('Content-Type: application/json; charset=utf-8');
+
+        $this->output->set_output(json_encode($this->map->guess($this->input->post())));
+    }
+
+}
