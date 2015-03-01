8782c80a6dbf82d23097125c1dd1a9029d07ae47
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-03-01 07:19:56 +0200

    Parsers: Removing the global configuration option 'full_path', ith depends solely on context.

--- platform/application/config/parser_cssmin.php
+++ platform/application/config/parser_cssmin.php
@@ -5,11 +5,10 @@
  * @license The MIT License, http://opensource.org/licenses/MIT
  */
 
+// See https://github.com/tubalmartin/YUI-CSS-compressor-PHP-port
+
 $config['raise_php_limits'] = TRUE;
 $config['memory_limit'] = '128M';
 $config['max_execution_time'] = 60;
 $config['pcre_backtrack_limit'] = 1000 * 1000;
 $config['pcre_recursion_limit'] = 500 * 1000;
-
-// This option enforces template full paths to be given for method parse().
-$config['full_path'] = FALSE;
