b002dd464523fe37da0c3e96c782cb3a1048b7f6
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-07-17 15:53:48 +0300

    Backporting changes from CodeIgniter 3.0.1-dev.

--- platform/application/config/database.php
+++ platform/application/config/database.php
@@ -32,17 +32,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |    ['char_set'] The character set used in communicating with the database
 |    ['dbcollat'] The character collation used in communicating with the database
 |                 NOTE: For MySQL and MySQLi databases, this setting is only used
-|                  as a backup if your server is running PHP < 5.2.3 or MySQL < 5.0.7
+|                 as a backup if your server is running PHP < 5.2.3 or MySQL < 5.0.7
 |                 (and in table creation queries made with DB Forge).
-|                  There is an incompatibility in PHP with mysql_real_escape_string() which
-|                  can make your site vulnerable to SQL injection if you are using a
-|                  multi-byte character set and are running versions lower than these.
-|                  Sites using Latin-1 or UTF-8 database character set and collation are unaffected.
+|                 There is an incompatibility in PHP with mysql_real_escape_string() which
+|                 can make your site vulnerable to SQL injection if you are using a
+|                 multi-byte character set and are running versions lower than these.
+|                 Sites using Latin-1 or UTF-8 database character set and collation are unaffected.
 |    ['swap_pre'] A default table prefix that should be swapped with the dbprefix
 |    ['encrypt']  Whether or not to use an encrypted connection.
+|
+|                     'mysql' (deprecated), 'sqlsrv' and 'pdo/sqlsrv' drivers accept TRUE/FALSE
+|                     'mysqli' and 'pdo/mysql' drivers accept an array with the following options:
+|
+|                         'ssl_key'    - Path to the private key file
+|                         'ssl_cert'   - Path to the public key certificate file
+|                         'ssl_ca'     - Path to the certificate authority file
+|                         'ssl_capath' - Path to a directory containing trusted CA certificats in PEM format
+|                         'ssl_cipher' - List of *allowed* ciphers to be used for the encryption, separated by colons (':')
+|                         'ssl_verify' - TRUE/FALSE; Whether verify the server certificate or not ('mysqli' only)
+|
 |    ['compress'] Whether or not to use client compression (MySQL only)
 |    ['stricton'] TRUE/FALSE - forces 'Strict Mode' connections
 |                            - good for ensuring strict SQL while developing
+|    ['ssl_options'] Used to set various SSL options that can be used when making SSL connections.
 |    ['failover'] array - A array with 0 or more data for connections if the main should fail.
 |    ['save_queries'] TRUE/FALSE - Whether to "save" all executed queries.
 |                 NOTE: Disabling this will also effectively disable both
