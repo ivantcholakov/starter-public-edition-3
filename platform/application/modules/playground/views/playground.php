3771111892f5d0b4fb3c844545c2fc19857d9fe9
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-11-10 16:44:55 +0200

    The Playground: Adding a test for Animate.css.

--- platform/application/modules/playground/views/playground.php
+++ platform/application/modules/playground/views/playground.php
@@ -77,7 +77,8 @@
                     <li><a href="<?php echo site_url('playground/typescript'); ?>">TypeScript Compiler Test</a></li>
                     <li><a href="<?php echo site_url('playground/colorbox'); ?>">Colorbox Test</a></li>
                     <li><a href="<?php echo site_url('playground/jsonmin'); ?>">JSON Minification Test</a></li>
-                </ul>
+                    <li><a href="<?php echo site_url('playground/animate'); ?>">Animate,css Test</a></li>
+               </ul>
 
             </div>
 
