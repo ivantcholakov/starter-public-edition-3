5cc4074d1efc31c7c1f8fb0d69754cac95b7fae1
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-04-20 02:54:21 +0300

    Updating the Thumbnail library, now it supports static thumbnails too.

--- platform/common/modules/thumbnail/controllers/Thumbnail_controller.php
+++ platform/common/modules/thumbnail/controllers/Thumbnail_controller.php
@@ -3,10 +3,6 @@
 /**
  * This controller outputs thumbnails.
  *
- * If watermaring is active and watermarks are to be applied selectively
- * based on image subdirectory, then this controller should be upgraded
- * with the needed additional logic.
- *
  * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2016
  * @license The MIT License, http://opensource.org/licenses/MIT
  */
@@ -23,16 +19,6 @@ class Thumbnail_controller extends CI_Controller {
         try {
 
             $this->load->library('thumbnail');
-
-            // An example for selective watermark activation.
-            $src = $this->input->get('src');
-            $path = str_replace(default_base_url(), '', $src);
-
-            if (strpos($path, 'assets/img/playground.jpg') === 0) {
-                $this->thumbnail->initialize(array('has_watermark' => true));
-            }
-            //
-
             $this->thumbnail->get($this->input->get());
 
         } catch (Exception $ex) {
