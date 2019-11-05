3b991647ee63f7cc2630bd9b5ad3109e1ea9c7da
Ivan Tcholakov <ivantcholakov@gmail.com>
2019-11-05 10:05:07 +0200

    composer update Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- platform/vendor/composer/autoload_static.php
+++ platform/vendor/composer/autoload_static.php
@@ -268,6 +268,7 @@ class ComposerStaticInit7ec88c9c84ecdbd075c7338e7e8fd784
         'GuzzleHttp\\Exception\\ClientException' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/Exception/ClientException.php',
         'GuzzleHttp\\Exception\\ConnectException' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/Exception/ConnectException.php',
         'GuzzleHttp\\Exception\\GuzzleException' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/Exception/GuzzleException.php',
+        'GuzzleHttp\\Exception\\InvalidArgumentException' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/Exception/InvalidArgumentException.php',
         'GuzzleHttp\\Exception\\RequestException' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/Exception/RequestException.php',
         'GuzzleHttp\\Exception\\SeekException' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/Exception/SeekException.php',
         'GuzzleHttp\\Exception\\ServerException' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/Exception/ServerException.php',
