018df57710e6f91998392ac4a9b6564a2ab598cd
Ivan Tcholakov <ivantcholakov@gmail.com>
2023-12-04 05:28:43 +0200

    composer update --ignore-platform-reqs

--- platform/vendor/composer/autoload_real.php
+++ platform/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit709299bed76b367f049a6f5f985c34d2
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit709299bed76b367f049a6f5f985c34d2', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit709299bed76b367f049a6f5f985c34d2', 'loadClassLoader'));
@@ -33,25 +31,18 @@ class ComposerAutoloaderInit709299bed76b367f049a6f5f985c34d2
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit709299bed76b367f049a6f5f985c34d2::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire709299bed76b367f049a6f5f985c34d2($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit709299bed76b367f049a6f5f985c34d2::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire709299bed76b367f049a6f5f985c34d2($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
